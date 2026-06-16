3d0c9125a84d408099cef0e064ce7a4114cb9537
H1pal <gmlvkf6666@gmail.com>
2026-06-16 10:20:15 +0900

    feat: hiltViewModel repository, DateStore 생성 및 구현을 통한 기본 테마 색 저장 뷰모델 생성과 테마 선택 화면 구현

--- app/src/main/java/com/example/todolist/TodoApplication.kt
+++ app/src/main/java/com/example/todolist/TodoApplication.kt
@@ -1,4 +1,7 @@
 package com.example.todolist
 
-class TodoApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApplication : Application()
