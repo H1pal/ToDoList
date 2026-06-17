218e8a1b4408a320d4440bb93eda9e959fd63b34
H1pal <gmlvkf6666@gmail.com>
2026-06-17 23:09:35 +0900

    feat: TopBar 생성

--- app/src/main/java/com/example/todolist/ui/components/TopBar.kt
+++ app/src/main/java/com/example/todolist/ui/components/TopBar.kt
@@ -7,26 +7,35 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.todolist.ui.theme.MainColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(
-    color: Color = MainColor,
-    titleText: String
+    titleText: String,
+    color: Color = MainColor
 ) {
     TopAppBar(
         title = {
             Text(
                 text = titleText,
                 fontWeight = FontWeight.W500,
-                fontSize = 44.sp
+                fontSize = 22.sp
             )
         },
         colors = topAppBarColors(
             containerColor = color,
             titleContentColor = Color.White
         )
+    )
+}
+
+@Preview
+@Composable
+fun PreviewTopBar() {
+    TopBar(
+        titleText = "Hello World",
     )
 }
